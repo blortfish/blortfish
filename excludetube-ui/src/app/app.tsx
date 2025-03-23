b95340ae081407a6d78209b979978da4b15b54cd
Daniel Flint <def6054@gmail.com>
2025-03-22 23:51:58 -0400

    filled in users and stuff

--- excludetube-ui/src/app/app.tsx
+++ excludetube-ui/src/app/app.tsx
@@ -1,24 +1,18 @@
 import PostList from './components/PostList';
 import './app.css';
-import { useEffect } from 'react';
+import {useEffect} from 'react';
 
 export function App() {
-  // Apply dark mode by default
-  useEffect(() => {
-    document.documentElement.classList.add('dark');
-  }, []);
+    // Apply dark mode by default
+    useEffect(() => {
+        document.documentElement.classList.add('dark');
+    }, []);
 
-  return (
-    <div className="dark:bg-dark-900 min-h-screen p-6 text-gray-800 dark:text-gray-200">
-      <div className="max-w-7xl mx-auto">
-        <h1 className="text-3xl font-bold mb-4 dark:text-gray-100">ExcludeTube UI</h1>
-        <div>
-          <h2 className="text-2xl font-semibold mb-6 dark:text-gray-200">Welcome to ExcludeTube</h2>
-          <PostList />
+    return (
+        <div className="dark:bg-dark-900 min-h-screen p-6 text-gray-800 dark:text-gray-200">
+            <PostList/>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ excludetube-ui/src/legacy-data/LegacyUsers.ts
@@ -0,0 +1,97 @@
+type LegacyUser = {
+    displayName: string;
+    id: string;
+    createdAt: string;
+}
+export const LegacyUsers: { [key: string]: LegacyUser } = {
+    "54982a544a9a94d61db6e6a3": {
+        "displayName": "Daddy King",
+        "id": "54982a544a9a94d61db6e6a3",
+        "createdAt": "2014-12-22T14:27:32.316Z"
+    },
+    "54982a604a9a94d61db6e6a4": {
+        "displayName": "Dan",
+        "id": "54982a604a9a94d61db6e6a4",
+        "createdAt": "2014-12-22T14:27:44.102Z"
+    },
+    "54982a664a9a94d61db6e6a5": {
+        "displayName": "Peanut Butter Princess",
+        "id": "54982a664a9a94d61db6e6a5",
+        "createdAt": "2014-12-22T14:27:50.930Z"
+    },
+    "54982a6e4a9a94d61db6e6a6": {
+        "displayName": "teeknow",
+        "id": "54982a6e4a9a94d61db6e6a6",
+        "createdAt": "2014-1a2-22T14:27:58.502Z"
+    },
+    "549b0fb3504a377d30f39410": {
+        "displayName": "kivo360",
+        "id": "549b0fb3504a377d30f39410",
+        "createdAt": "2014-12-24T19:10:43.771Z"
+    },
+    "549b58d1504a377d30f39417": {
+        "displayName": "Feee",
+        "id": "549b58d1504a377d30f39417",
+        "createdAt": "2014-12-25T00:22:41.864Z"
+    },
+    "54be9b5d045c6d291d510c6d": {
+        "displayName": "Camille",
+        "id": "54be9b5d045c6d291d510c6d",
+        "createdAt": "2015-01-20T18:15:57.618Z"
+    },
+    "54f937abfeeeb9fc216e7e79": {
+        "displayName": "Lisa",
+        "id": "54f937abfeeeb9fc216e7e79",
+        "createdAt": "2015-03-06T05:14:19.397Z"
+    },
+    "5514c554feeeb9fc216e7ea1": {
+        "displayName": "Anthony",
+        "id": "5514c554feeeb9fc216e7ea1",
+        "createdAt": "2015-03-27T02:49:56.765Z"
+    },
+    "559d8f039c01e2411f168b37": {
+        "displayName": "kflint530",
+        "id": "559d8f039c01e2411f168b37",
+        "createdAt": "2015-07-08T20:58:43.552Z"
+    },
+    "55db294a9c01e2411f168b79": {
+        "displayName": "SquattingDick",
+        "id": "55db294a9c01e2411f168b79",
+        "createdAt": "2015-08-24T14:25:14.507Z"
+    },
+    "5617d1b926496f29106f4c7a": {
+        "displayName": "kdog",
+        "id": "5617d1b926496f29106f4c7a",
+        "createdAt": "2015-10-09T14:39:53.322Z"
+    },
+    "56b4e6085a5b6a2f6a38f72b": {
+        "displayName": "Khilljoy",
+        "id": "56b4e6085a5b6a2f6a38f72b",
+        "createdAt": "2016-02-05T18:12:24.397Z"
+    },
+    "56cfa474632925492c907657": {
+        "displayName": "Kevin",
+        "id": "56cfa474632925492c907657",
+        "createdAt": "2016-02-26T01:03:48.304Z"
+    },
+    "5818ef5d6c95cdc01c4e5193": {
+        "displayName": "pickle",
+        "id": "5818ef5d6c95cdc01c4e5193",
+        "createdAt": "2016-11-01T19:39:09.335Z"
+    },
+    "58e0661de3396128296e72ec": {
+        "displayName": "CockRing",
+        "id": "58e0661de3396128296e72ec",
+        "createdAt": "2017-04-02T02:46:53.947Z"
+    },
+    "5ac27d4d7dea77d656a5a497": {
+        "displayName": "blortfish",
+        "id": "5ac27d4d7dea77d656a5a497",
+        "createdAt": "2018-04-02T18:58:21.175Z"
+    },
+    "5e90c458ee7bfa462fdc0cf2": {
+        "displayName": "theRyno",
+        "id": "5e90c458ee7bfa462fdc0cf2",
+        "createdAt": "2020-04-10T19:09:12.792Z"
+    }
+}
