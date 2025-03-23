65326edc77a306ceb21f4f5e59cb2e701e509d35
Daniel Flint <def6054@gmail.com>
2025-03-22 22:32:52 -0400

    wip

---
+++ excludetube-ui/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'excludetube-ui',
+  preset: '../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../coverage/excludetube-ui',
+};
